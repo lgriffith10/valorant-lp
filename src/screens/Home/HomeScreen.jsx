de03ebd0edb88e273b68c993288bb844cb649efc
lgriffithdev <lgriffith.dev@gmail.com>
2023-02-16 22:08:49 +0100

    feat: homeScreen styling

--- src/screens/Home/HomeScreen.jsx
+++ src/screens/Home/HomeScreen.jsx
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native';
+import { 
+    View, 
+    Text, 
+    Image, 
+    StyleSheet 
+} from 'react-native';
 
 
 const HomeScreen = () => {
     return (
-        <View>
-            <Text>Hello homepage</Text>
+        <View style={styles.container}>
+            <Image resizeMode='contain' style={styles.image} source={require('../../img/valorant.png')} />
+            <Text style={styles.text}>Welcome on the Valorant App</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#1b1b1b',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: 200,
+    },
+    text: {
+        color: 'white',
+        fontSize: 24,
+        fontWeight: 'bold',
+    }
+});
 
 export default HomeScreen;
